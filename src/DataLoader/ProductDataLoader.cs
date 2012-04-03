0a54030a83c2ab75fdb2b18278001db16ccf67d9
Alonso Robles <alonso@headspring.com>
2012-04-03 15:39:12 -0500

    Fixed image path in product data loader. Added feature to create a cart and add items to it.

--- src/DataLoader/ProductDataLoader.cs
+++ src/DataLoader/ProductDataLoader.cs
@@ -15,7 +15,7 @@ namespace Depot.DataLoader
                                                              Description =
                                                                  @"<p><em>Pragmatic Project Automation</em> shows you how to improve the consistency and repeatability of your project's procedures using automation to reduce risk and errors.</p><p>Simply put, we're going to put this thing called a computer to work for you doing the mundane (but important) project stuff. That means you'll have more time and energy to do the really exciting---and difficult---stuff, like writing quality code.</p>",
                                                              Id = 1,
-                                                             ImageUrl = "/images/auto.jpg",
+                                                             ImageUrl = "/Content/images/auto.jpg",
                                                              Price = 29.95m
                                                          },
                                                      new Product
@@ -24,7 +24,7 @@ namespace Depot.DataLoader
                                                              Description =
                                                                  @"<p>This book is a recipe-based approach to using Subversion that will get you up and running quickly---and correctly. All projects need version control: it's a foundational piece of any project's infrastructure. Yet half of all project teams in the U.S. don't use any version control at all. Many others don't use it well, and end up experiencing time-consuming problems.</p>",
                                                              Id = 2,
-                                                             ImageUrl = "/images/svn.jpg",
+                                                             ImageUrl = "/Content/images/svn.jpg",
                                                              Price = 28.50m
                                                          },
                                                      new Product
@@ -33,7 +33,7 @@ namespace Depot.DataLoader
                                                              Description =
                                                                  @"<p>Pragmatic programmers use feedback to drive their development and personal processes. The most valuable feedback you can get while coding comes from unit testing.</p><p>Without good tests in place, coding can become a frustrating game of ""whack-a-mole."" That's the carnival game where the player strikes at a mechanical mole; it retreats and another mole pops up on the opposite side of the field. The moles pop up and down so fast that you end up flailing your mallet helplessly as the moles continue to pop up where you least expect them.</p>",
                                                              Id = 3,
-                                                             ImageUrl = "/images/utc.jpg",
+                                                             ImageUrl = "/Content/images/utc.jpg",
                                                              Price = 27.75m
                                                          }
                                                  };
--- src/Web/Controllers/StoreController.cs
+++ src/Web/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Depot.Web.Filters;
 using Depot.Web.Models;
@@ -21,6 +22,37 @@ namespace Depot.Web.Controllers
         {
             var products = _session.Query<Product>().OrderBy(product => product.Title).ToArray();
             return AutoMapView<ProductListModel[]>(products, View());
+        }
+
+        private Cart FindCart()
+        {
+            if (Session["Cart"] == null)
+                Session["Cart"] = new Cart();
+            return Session["Cart"] as Cart;
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int id)
+        {
+            var product = _session.Load<Product>(id);
+            var cart = FindCart();
+            cart.AddProduct(product);
+            return View(cart);
+        }
+    }
+
+    public class Cart
+    {
+        public Cart()
+        {
+            Items = new List<Product>();
+        }
+
+        public IList<Product> Items { get; private set; }
+
+        public void AddProduct(Product product)
+        {
+            Items.Add(product);
         }
     }
 }
