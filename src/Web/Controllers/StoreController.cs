099a76365bc1429b88dff38e6bcfa50d3e246820
Alonso Robles <alonso@headspring.com>
2012-04-03 16:29:39 -0500

    Added cart error handling and re organized the view layouts.

--- src/Web/Controllers/StoreController.cs
+++ src/Web/Controllers/StoreController.cs
@@ -2,9 +2,11 @@
 using System.Linq;
 using System.Web.Mvc;
 using Depot.Web.Filters;
+using Depot.Web.Helpers;
 using Depot.Web.Models;
 using Raven.Client;
 using Raven.Client.Linq;
+using Depot.Web.Extensions;
 
 namespace Depot.Web.Controllers
 {
@@ -31,10 +33,11 @@ namespace Depot.Web.Controllers
             return Session["Cart"] as Cart;
         }
 
-        [HttpPost]
         public ActionResult AddToCart(int id)
         {
             var product = _session.Load<Product>(id);
+            if (product == null)
+                return RedirectToAction("Index").WithFlash(FlashMessageType.Error, "Product was not found");
             var cart = FindCart();
             cart.AddProduct(product);
             return View(cart);
